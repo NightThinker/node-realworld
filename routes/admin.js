5e1ea4b429c918beffe68739b6053158755958eb
May <may@Gato-Pro.local>
2019-01-30 16:21:57 +0700

    Filtering Paths

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,13 @@ const express = require('express')
 
 const router = express.Router()
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   console.log("In another Middleware product")
-  res.send('<form action="/product" method="POST" ><input type="text" name="title"><button type="submit">Add Product</button></form>')
+  res.send('<form action="/admin/product" method="POST" ><input type="text" name="title"><button type="submit">Add Product</button></form>')
 })
 
+// /admin/product => POST
 router.post('/product', (req, res,next) => {
   console.log('body : ', req.body)
   res.redirect('/')
