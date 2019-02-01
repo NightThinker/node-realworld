b375476baf163991a2727d5b5d2f2624c7a97632
May <may@Gato-Pro.local>
2019-02-02 01:50:34 +0700

    Installing & Implementing Pug

--- routes/shop.js
+++ routes/shop.js
@@ -7,8 +7,9 @@ const adminData = require('./admin')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-  console.log('admin data  products :', adminData.products)
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+  // console.log('admin data  products :', adminData.products)
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+  res.render('shop')
 })
 
 module.exports = router
