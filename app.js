93766cd31a02436dbf57c839c16c4b063a74e31a
May <may@Gato-Pro.local>
2019-02-02 22:14:04 +0700

    Working on the Layout with Partials

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(shopRoutes)
 
 
 app.use((req, res, next) => {
-  res.status(404).render('404', {pageTitle: 'Page not found'})
+  res.status(404).render('404', {pageTitle: 'Page not found', path: ''})
   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
