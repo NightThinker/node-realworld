8377689451265c356965945cb89bb3441451df5b
May <may@Gato-Pro.local>
2019-02-02 21:39:07 +0700

    Working with EJS

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
-const expressHds = require('express-handlebars')
+//hbs
+// const expressHds = require('express-handlebars')
 
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const app = express()
 
-app.engine('hbs', expressHds({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}))
+//hbs
+// app.engine('hbs', expressHds({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}))
 // app.set('view engine', 'pug')
-app.set('view engine', 'hbs')
+// app.set('view engine', 'hbs')
+app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({extended: false}))
