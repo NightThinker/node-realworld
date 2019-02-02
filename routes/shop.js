8d2e5fd1fe941105ef6d1814e3749ec9be8c36eb
May <may@Gato-Pro.local>
2019-02-02 14:34:37 +0700

    Outputting Dynamic Content

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,8 @@ const router = express.Router()
 router.get('/', (req, res, next) => {
   // console.log('admin data  products :', adminData.products)
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
-  res.render('shop')
+  const products = adminData.products
+  res.render('shop', {prods: products, docTitle: 'shop'})
 })
 
 module.exports = router
