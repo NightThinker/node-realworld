9286efd2115700cf1f02af5e7f6020bee5ea4bfb
May <may@Gato-Pro.local>
2019-02-06 14:55:31 +0700

    Passing Data with POST Requests

--- routes/shop.js
+++ routes/shop.js
@@ -6,10 +6,15 @@ const shopControllers = require('../controllers/shop')
 const router = express.Router()
 
 router.get('/', shopControllers.getIndex)
+
 router.get('/products', shopControllers.getProducts)
 router.get('/products/:productId', shopControllers.getProduct)
+
 router.get('/cart', shopControllers.getCart)
+router.post('/cart', shopControllers.postCart)
+
 router.get('/orders', shopControllers.getOrders)
+
 router.get('/checkout', shopControllers.getCheckout)
 
 module.exports = router
