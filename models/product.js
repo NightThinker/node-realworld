6ed56814f6698fa62dc8232b03092d897a6988fd
May <may@Gato-Pro.local>
2019-02-09 01:56:43 +0700

    Fetching Products

--- models/product.js
+++ models/product.js
@@ -1,19 +1,6 @@
-const path = require('path')
-const fs = require('fs')
+const db = require('../util/database')
+
 const Cart = require('./cart')
-
-// const products = []
-const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
-
-const getProductFromFile = callback => {
-  fs.readFile(p, (err, fileContent) => {
-    if(err) {
-      callback([])
-    } else {
-      callback(JSON.parse(fileContent))
-    }
-  })
-}
 
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
@@ -25,47 +12,18 @@ module.exports = class Product {
   }
 
   save() {
-    // products.push(this)
-    
-    getProductFromFile(products => {
-      if(this.id) {
-        const existingProductIndex = products.findIndex(prod => prod.id === this.id)
-        const updatedProducts = [...products]
-        updatedProducts[existingProductIndex] = this
-        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-          console.log(err)
-        })
-      } else {
-        this.id = Math.random().toString()
-        products.push(this)
-        fs.writeFile(p, JSON.stringify(products), err => {
-          console.log(err)
-        })
-      }
-    })
   }
 
   static deleteById(id) {
-    getProductFromFile(products => {
-      const product = products.find(prod => prod.id === id)
-      const updatedProduct = products.filter(prod => prod.id !== id)
-      fs.writeFile(p, JSON.stringify(updatedProduct), err => {
-        if(!err) {
-          Cart.deleteProduct(id, product.price)
-        }
-      })
-    })
+
   }
 
-  static fetchAll(callback) {
-    getProductFromFile(callback)
+  static fetchAll() {
+   return db.execute('SELECT * FROM products')
   }
 
-  static findById(id, callback) {
-    getProductFromFile(products => {
-      const product = products.find(p => p.id === id)
-      callback(product)
-    })
+  static findById(id) {
+    
   }
 
 }
