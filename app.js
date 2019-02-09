1e918e38caa252cb7af39f7adbaa42456334e6ba
May <may@Gato-Pro.local>
2019-02-09 17:53:58 +0700

    Adding a One-To-Many Relationship

--- app.js
+++ app.js
@@ -2,16 +2,13 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const sequelize = require('./util/database')
-//hbs
-// const expressHds = require('express-handlebars')
 const errorControllers = require('./controllers/error')
+
+const Product = require('./models/product')
+const User = require('./models/user')
 
 const app = express()
 
-//hbs
-// app.engine('hbs', expressHds({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}))
-// app.set('view engine', 'pug')
-// app.set('view engine', 'hbs')
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 const adminRoutes = require('./routes/admin')
@@ -23,10 +20,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
-
 app.use(errorControllers.get404)
 
-sequelize.sync()
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
+User.hasMany(Product)
+
+sequelize
+  .sync({ force: true })
   .then(result => {
     // console.log('result : ', result)
     app.listen(3000);
