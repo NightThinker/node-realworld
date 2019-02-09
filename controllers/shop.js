dd0e8afb74ad179e8f3fd3c46bec137c9d601111
May <may@Gato-Pro.local>
2019-02-09 16:49:23 +0700

    Getting a Single Product with the "where" Condition

--- controllers/shop.js
+++ controllers/shop.js
@@ -19,15 +19,25 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId
-  Products.findById(prodId)
-    .then(([product]) => {
+  Products.findAll({where: {id: prodId}})
+    .then(products => {
       res.render('shop/product-detail', {
-        product: product[0],
-        pageTitle: product.title,
+        product: products[0],
+        pageTitle: products[0].title,
         path: '/products'
       })
     })
-    .catch(err => console.log('err : ', err))
+    .catch(err => console.log(err))
+    // ---- or -----
+  // Products.findById(prodId)
+  //   .then(product => {
+  //     res.render('shop/product-detail', {
+  //       product: product,
+  //       pageTitle: product.title,
+  //       path: '/products'
+  //     })
+  //   })
+  //   .catch(err => console.log('err : ', err))
 }
 
 exports.getIndex = (req, res, next) => {
