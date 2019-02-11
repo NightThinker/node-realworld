3771490f9778270da7ba923ef3769626441bda86
May <may@Gato-Pro.local>
2019-02-11 15:26:11 +0700

    Creating Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,4 @@
-const Products = require('../models/product')
+const Product = require('../models/product')
 
 exports.getAddProduct = (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
@@ -15,14 +15,9 @@ exports.postAddProduct = (req, res,next) => {
   const imageUrl = req.body.imageUrl
   const description = req.body.description
   const price = req.body.price
-  req.user
-    .createProduct({
-      title: title,
-      price: price,
-      imageUrl: imageUrl,
-      description: description,
-      userId: req.user.id
-    })
+  const product = new Product(title, price, description, imageUrl)
+  product
+    .save()
     .then(result => {
       // console.log('result: ', result)
       console.log('created Product')
