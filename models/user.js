8b42e88c45216ab0cc845662d103400cccc507fd
May <may@Gato-Pro.local>
2019-02-15 12:38:41 +0700

    updatedCartItem

--- models/user.js
+++ models/user.js
@@ -70,6 +70,19 @@ class User {
       .catch(err => console.log(err))
   }
 
+  deleteItemFromCart(productId) {
+    const updatedCartItem = this.cart.items.filter(item => {
+      return item.productId.toString() !== productId.toString()
+    })
+    const db = getDb()
+    return db
+      .collection('users')
+      .updateOne(
+        {_id: new ObjectId(this._id)}, 
+        { $set: { cart: {items: updatedCartItem} }}
+      )
+  }
+
   static findById(userId) {
     const db = getDb()
     return db.collection('users')
