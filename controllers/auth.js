55e195377bf3a6dd1cab0ff0ebd5b1d5fc600d07
May <may@Gato-Pro.local>
2019-02-26 17:32:06 +0700

    nUsing the Session Middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,13 @@
 
 exports.getLogin = (req, res, next) => {
-  const isLoginedIn = req.get('Cookie').split('=')[1] === 'true'
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoginedIn
+    isAuthenticated: true
   })
 }
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly')
+  req.session.isLoginedIn = true
   res.redirect('/')
 }
