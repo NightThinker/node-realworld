de0a429fa427005263e584040aeb4400090be49a
May <may@Gato-Pro.local>
2019-02-26 16:33:44 +0700

    Creating the Login Form

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set('view engine', 'ejs')
 app.set('views', 'views')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+const authRoutes = require('./routes/auth')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
+app.use(authRoutes)
 
 app.use(errorControllers.get404)
 
---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const express = require('express')
+
+const authControllers = require('../controllers/auth')
+
+const router = express.Router()
+
+router.get('/login', authControllers.getLogin)
+
+module.exports = router
