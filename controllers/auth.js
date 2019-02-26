97e7589a3b17c18dd0278f30c88fb588176a4f59
May <may@Gato-Pro.local>
2019-02-26 16:53:11 +0700

    Adding the Request Driven Login Solution

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,12 @@
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
-    pageTitle: 'Login'
+    pageTitle: 'Login',
+    isAuthenticated: req.isLoginedIn
   })
+}
+
+exports.postLogin = (req, res, next) => {
+  req.isLoginedIn = true
+  res.redirect('/')
 }
--- routes/auth.js
+++ routes/auth.js
@@ -6,5 +6,6 @@ const authControllers = require('../controllers/auth')
 const router = express.Router()
 
 router.get('/login', authControllers.getLogin)
+router.post('/login', authControllers.postLogin)
 
 module.exports = router
