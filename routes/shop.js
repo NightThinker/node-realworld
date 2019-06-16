20d58748a54b8956202e0ec22f6d82ea27ed5dd5
May <may@Gato-Pro.local>
2019-06-16 15:56:15 +0700

    Using Middleware to Protect Routes

--- routes/shop.js
+++ routes/shop.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express =  require('express')
 
 const shopControllers = require('../controllers/shop')
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router()
 
@@ -10,14 +11,14 @@ router.get('/', shopControllers.getIndex)
 router.get('/products', shopControllers.getProducts)
 router.get('/products/:productId', shopControllers.getProduct)
 
-router.get('/cart', shopControllers.getCart)
-router.post('/cart', shopControllers.postCart)
+router.get('/cart', isAuth, shopControllers.getCart)
+router.post('/cart', isAuth, shopControllers.postCart)
 
-router.post('/cart-delete-item', shopControllers.postCartDeleteProduct)
+router.post('/cart-delete-item', isAuth, shopControllers.postCartDeleteProduct)
 
-router.post('/create-order', shopControllers.postOrder)
+router.post('/create-order', isAuth, shopControllers.postOrder)
 
-router.get('/orders', shopControllers.getOrders)
+router.get('/orders', isAuth, shopControllers.getOrders)
 
 // router.get('/checkout', shopControllers.getCheckout)
 
