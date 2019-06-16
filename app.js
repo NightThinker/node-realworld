30540bb270e334f702bc19c1e1415ec062cf17bd
May <may@Gato-Pro.local>
2019-06-16 16:14:54 +0700

    Using a CSRF Token

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const session = require('express-session')
 const MongoDBStore = require('connect-mongodb-session')(session)
+const csrf = require('csurf');
 
 const errorControllers = require('./controllers/error')
 const User = require('./models/user')
@@ -17,6 +18,8 @@ const store = new MongoDBStore({
   collection: 'sessions'
 })
 
+const csrfProtection = csrf();
+
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 const adminRoutes = require('./routes/admin')
@@ -25,7 +28,8 @@ const authRoutes = require('./routes/auth')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}))
+app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}));
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
   console.log(req.session.user)
