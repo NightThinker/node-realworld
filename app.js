06e6864fa5474b6b518d9e883e04ecdd7a4faa38
NightThinker <travel_special@hotmail.com>
2019-06-22 23:30:27 +0700

    Throwing Errors in Code

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
-const path = require('path')
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const session = require('express-session')
-const MongoDBStore = require('connect-mongodb-session')(session)
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
 
-const errorControllers = require('./controllers/error')
-const User = require('./models/user')
+const errorControllers = require('./controllers/error');
+const User = require('./models/user');
 
-const MONGODB_URI = 'mongodb+srv://may:9tvmwpZzSvt5hgKv@cluster0-rnstb.mongodb.net/shop'
+const MONGODB_URI = 'mongodb+srv://may:9tvmwpZzSvt5hgKv@cluster0-rnstb.mongodb.net/shop';
 
-
-const app = express()
+const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
-})
+});
 
 const csrfProtection = csrf();
 
-app.set('view engine', 'ejs')
-app.set('views', 'views')
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
-const authRoutes = require('./routes/auth')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
-    secret: 'my secret', 
-    resave: false, 
-    saveUninitialized: false, 
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
     store: store
   })
 );
@@ -41,35 +40,40 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
-  console.log(req.session.user)
-  if(!req.session.user) {
-    return next()
+  console.log(req.session.user);
+  if (!req.session.user) {
+    return next();
   }
   User.findById(req.session.user._id)
-  .then(user => {
-    req.user = user
-    next()
-  })
-  .catch(err => console.log(err));
-})
+    .then((user) => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+});
 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.csrfToken = req.csrfToken();
   next();
-})
+});
 
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
-app.use(authRoutes)
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
 
-app.use(errorControllers.get404)
+app.use(errorControllers.get404);
 
 mongoose
   .connect(MONGODB_URI)
-  .then(result => {
-    app.listen(3000)
+  .then((result) => {
+    app.listen(3000);
   })
-  .catch(err=> {
-    console.log(err)
-  })
+  .catch((err) => {
+    console.log(err);
+  });
