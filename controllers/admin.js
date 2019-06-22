cd99094e4627d082143ffbed7cd3f315822da8c7
NightThinker <travel_special@hotmail.com>
2019-06-22 19:35:30 +0700

    Adding Authorization

--- controllers/admin.js
+++ controllers/admin.js
@@ -84,7 +84,7 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product
-    .find()
+    .find({userId: req.user._id})
     // .select('title price -_id')
     // .populate('userId', 'name')
     .then(products => {
