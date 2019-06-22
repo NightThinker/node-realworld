187cf13ea800b507111925a77a6b633210ae7ce6
NightThinker <travel_special@hotmail.com>
2019-06-22 21:16:19 +0700

    Checking For Field Equality confirmpassword

--- routes/auth.js
+++ routes/auth.js
@@ -23,7 +23,13 @@ router.post(
       }),
     body('password', 'Please enter a password with only numbers and text and at least 5 characters.')
       .isLength({ min: 5 })
-      .isAlphanumeric()
+      .isAlphanumeric(),
+    body('confirmPassword').custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Password have to match!');
+      }
+      return true;
+    })
   ],
   authControllers.postSignup
 );
