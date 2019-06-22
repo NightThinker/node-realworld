52eb5c3e7d76ee8bda74d80a15041881e5330c74
NightThinker <travel_special@hotmail.com>
2019-06-22 20:59:53 +0700

    Built-In & Custom Validators

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,34 @@
-const express = require('express')
+const express = require('express');
 const { check } = require('express-validator/check');
 
-const authControllers = require('../controllers/auth')
+const authControllers = require('../controllers/auth');
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/login', authControllers.getLogin)
-router.post('/login', authControllers.postLogin)
+router.get('/login', authControllers.getLogin);
+router.post('/login', authControllers.postLogin);
 
 router.get('/signup', authControllers.getSignup);
-router.post('/signup', check('email').isEmail().withMessage('Please enter a vaild email.'), authControllers.postSignup);
+router.post(
+  '/signup',
+  check('email')
+    .isEmail()
+    .withMessage('Please enter a vaild email.')
+    .custom((value, { req }) => {
+      if (value === 'test@test.com') {
+        throw new Error('This email address if forbidden.');
+      }
+      return true;
+    }),
+  authControllers.postSignup
+);
 
-router.post('/logout', authControllers.postLogout)
+router.post('/logout', authControllers.postLogout);
 
-router.get('/reset', authControllers.getReset)
-router.post('/reset', authControllers.postReset)
+router.get('/reset', authControllers.getReset);
+router.post('/reset', authControllers.postReset);
 
-router.get('/reset/:token', authControllers.getNewPassword)
-router.post('/new-password', authControllers.postNewPassword)
+router.get('/reset/:token', authControllers.getNewPassword);
+router.post('/new-password', authControllers.postNewPassword);
 
-module.exports = router
+module.exports = router;
