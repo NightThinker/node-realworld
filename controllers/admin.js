3da3b9aa1d422735c3ca8613dcbf3e596a2132a1
NightThinker <travel_special@hotmail.com>
2019-06-22 19:46:30 +0700

    Adding Protection to Post Actions

--- controllers/admin.js
+++ controllers/admin.js
@@ -65,17 +65,17 @@ exports.postEditProduct = (req, res, next) => {
   Product
     .findById(prodId)
     .then(product => {
+      if (product.userId.toString() !== req.user._id.toString()) {
+        return res.redirect('/');
+      }
       product.title = updatedTitle
       product.price = updatedPrice
       product.description = updatedDescription
       product.imageUrl = updatedImageUrl
-      return product.save()
-    })
-    .catch(err => console.log(err))
-  
-    .then(result => {
-      console.log('Update Product')
-      res.redirect('/admin/products')
+      return product.save().then(result => {
+        console.log('Update Product')
+        res.redirect('/admin/products')
+      }).catch(err => console.log(err))
     })
     .catch(err => console.log(err))
 }
@@ -100,7 +100,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId
-  Product.findByIdAndRemove(prodId)
+  Product.deleteOne({_id: prodId, userId: req.user._id})
     .then(() => {
       console.log('Delete Product')
       res.redirect('/admin/products')
